44dd4bd5d3c63288ffaccc47128914a915d4b06d
ugbeadie <ugbeadie3@gmail.com>
2023-02-06 17:37:40 +0100

    style input fields

--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ function App() {
     <div className='info_field'>
       <Input name='CARDHOLDER NAME'placeholder='e.g. Jane Appleseed'/>
       <Input name='CARD NUMBER' placeholder='e.g. 1234 5678 9123 0000'/>
+      <div className='other'>
+        <div className='exp_date'>
+          <h3>EXP. DATE(MM/YY)</h3>
+          <div>
+          <input placeholder='MM'/>
+          <input placeholder='YY'/>
+          </div>
+        </div>
+        <div className='cvc'>
+          <h3>CVC</h3>
+          <input placeholder='e.g. 123'/>
+        </div>
+      </div>
     </div>
   );
 }
